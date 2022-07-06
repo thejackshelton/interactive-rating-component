76ac90e0bb77a3f1020934d8c32748d3fd776c63
thejackshelton <jacksheltontx@outlook.com>
2022-07-05 20:19:02 -0500

    ChangesV2

---
+++ app.js
@@ -0,0 +1,19 @@
+const buttonRating = document.querySelectorAll('.ratings');
+let ratings = [];
+let lastRating = 0;
+
+//Selects each button
+buttonRating.forEach((button) => {
+    button.addEventListener('click', () => {
+        let index = parseInt(button.getAttribute("data-rate"));
+        button.classList.toggle('selected');
+        console.log(index);
+
+
+        ratings.push(index);
+        for (let i = 0; i < ratings.length; i++) {
+            lastRating = ratings.at(-1);
+            button.classList.remove('selected')
+        }
+    })
+})
